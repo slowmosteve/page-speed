a52e1f60d28429c6f75a1c369c36a94350253533
Steve Choi <steve.choi@telus.com>
2018-11-01 09:52:11 -0400

    Initial commit

---
+++ gcf-pubsub-to-bq/pubsub-to-bq.py
@@ -0,0 +1,34 @@
+import base64
+import json
+from google.cloud import bigquery
+
+dataset_id = 'page_speed_reports'
+table_id = 'reports'
+
+def pubsub_bq_insert(event, dataset, table):
+    """Cloud function triggered from a message on a Cloud Pub/Sub topic.
+    Decodes data payload from message and inserts into BigQuery table.
+
+    Args:
+        event (dict): Event payload.
+        dataset: BigQuery dataset id
+        table: BigQuery table id
+    """
+    print("event: "+str(event))
+    print("context: "+str(context))
+    raw_data = event['data']
+    stream = base64.urlsafe_b64decode(raw_data)
+    decoded_stream = base64.urlsafe_b64decode(stream)
+    pubsub_message = json.loads(decoded_stream)
+    print("data to insert :"+str(pubsub_message))
+
+    client = bigquery.Client()
+    table_ref = client.dataset(dataset_id).table(table_id)
+    table = client.get_table(table_ref)
+
+    rows_to_insert = [pubsub_message]
+
+    errors = client.insert_rows(table, rows_to_insert)
+    if (errors != []):
+        print(errors)
+    assert errors == []
